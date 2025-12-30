ce13d90e6d1a9dd0548c9f4460fff9efeab08c2e
Norah Alnaif <norahalnaif@MacBook-Air-Norah.local>
2025-12-30 15:58:19 +0300

    Updat

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // ابحثي عن أي سطر فيه http://localhost:5000 واستبدليه بهذا:
-const API_BASE_URL = "https://sunnaapp.onrender.com/api";
+const API_BASE_URL = " http://127.0.0.1:8080/api";
 
 // مثال لعملية تسجيل أو جلب بيانات:
 fetch(`${API_BASE_URL}/Auth/social-links`)
